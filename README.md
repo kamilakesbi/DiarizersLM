08e97f08787a183c22a154dc38854323e0d5d5cb
kamil@huggingface.co <kamilakesbi@gmail.com>
2024-06-04 10:47:05 +0000

    construct prompts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DiarizersLM
+
+
+## prepare_data: 
+
+```
+python3 train_data_prep.py \
+--input="example_data.json" \
+--output="example_data_processed.json" \
+--output_type=json \
+--emit_input_length=1000 \
+--emit_target_length=1000 \
+--prompt_suffix=" --> " \
+--completion_suffix=" [eod]" \
+--input_feature_key='prompt' \
+--output_feature_key='completion'
+```
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import torch
 from asr_diarize import ASRDiarizationPipeline
 from datasets import load_dataset
-import diarizationlm 
-
+from diarizationlm import utils
+import diarizationlm
 
 device = "cuda:2" if torch.cuda.is_available() else "cpu"
 pipeline = ASRDiarizationPipeline.from_pretrained("openai/whisper-tiny", device=device)
@@ -12,8 +12,20 @@ dataset = load_dataset("diarizers-community/ami",'ihm', split="train", streaming
 # get first sample
 sample = next(iter(dataset))
 
-sample['audio']['array'] = sample['audio']['array'][:3*60*16000]
+sample['audio']['array'] = sample['audio']['array'][:2*60*16000]
 
 src_text, src_spk = pipeline(sample["audio"])
 
-result = diarizationlm.create_diarized_text(src_text.split(' '), src_spk.split(' '))
+utt = {"utterance_id": "0",  "hyp_text": str(src_text) , "hyp_spk": src_spk}
+
+po = utils.PromptOptions()
+
+prompts = diarizationlm.generate_prompts(utt, po)
+
+print(prompts)
+
+
+
+# result = diarizationlm.create_diarized_text(src_text.split(' '), src_spk.split(' '))
+
+
