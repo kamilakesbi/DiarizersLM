8f7021fd09cb334c3f67843acc1753f7c9eac7cf
kamilakesbi <kamilakesbi@users.noreply.huggingface.co>
2024-06-13 17:40:42 +0200

    clean script

--- preprocessing/run.py
+++ preprocessing/run.py
@@ -4,7 +4,6 @@ import torch
 from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperTokenizer
 from pyannote.audio import Pipeline
 
-from transformers.utils import is_torch_sdpa_available 
 from accelerate import Accelerator
 from torch.utils.data import DataLoader
 from utils import add_batch_to_dataset, DataCollatorAudio, DataCollatorLabels
@@ -12,7 +11,87 @@ from processor import Processor
 from tqdm import tqdm 
 import logging 
 import time
+from dataclasses import dataclass, field
+from transformers import HfArgumentParser
+from typing import Optional
 
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to distill from.
+    """
+    asr_name_or_path: str = field(
+        metadata={"help": "Path to pretrained Whisper model or model identifier from huggingface.co/models"}
+    )
+    diarizer_name_or_path: str = field(
+        metadata={"help": "Path to pretrained Pyannote model or model identifier from huggingface.co/models"}
+    )
+
+    attn_implementation: Optional[str] = field(
+            default=None,
+            metadata={
+                "help": (
+                    "Which attention implementation to use in the encoder and decoder attention layers. Can be one of:\n"
+                    "1. `eager` or `None`: default Transformers attention implementation.\n"
+                    "2. `sdpa`: Flash Attention through PyTorch SDPA. Requires `torch>=2.1`. Recommended for hardware where Flash Attention 2 is not supported, e.g. Turing GPUs, (T4, RTX 2080).\n"
+                    "3. `flash_attn_2`: Flash Attention 2 through the Flash Attention package https://github.com/Dao-AILab/flash-attention. **Always** recommended on supported hardware (Ampere, Ada, or Hopper GPUs, e.g., A100, RTX 3090, RTX 4090, H100)."
+                )
+            },
+        )
+
+    
+@dataclass
+class DataArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to distill from.
+    """
+    dataset_name: str = field(
+        default=None,
+        metadata={"help": "The name of the dataset to use (via the datasets library)."},
+    )
+
+    dataset_split_name: str = field(
+        default="train",
+        metadata={
+            "help": (
+                "The name of the data set splits to use (via the datasets library)."
+                " Defaults to 'train'. Multiple splits can be passed by splitting a"
+                " list through the '+' character, e.g. 'train+validation' will"
+                " pseudo-label both the 'train' and 'validation' splits sequentially."
+            )
+        },
+    )
+
+    streaming: bool = field(
+        default=False,
+        metadata={"help": "Whether to use dataset's streaming mode to load and pre-process the data."},
+    )
+
+    per_device_batch_size: int = field(
+        default=None,
+        metadata={"help": "Per device batch size used by the dataloader."},
+    )
+
+    dataloader_num_workers: int = field(
+        default=None,
+        metadata={"help": "Number of workers used in the Dataloader"},
+    )
+
+    num_proc: int = field(
+        default=None,
+        metadata={"help": "Number of workers used to load the dataset"},
+    )
+
+    push_to_hub: bool = field(
+        default=True,
+        metadata={"help": "Whether to push the processed dataset to the Hub or not."},
+    )
+
+    output_hub_repository: str= field(
+
+        metadata={"help": "Hub repository"},
+    )
 
 if __name__ == '__main__': 
 
@@ -25,46 +104,54 @@ if __name__ == '__main__':
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-    # Hyperparameters: 
-    dataset_name = 'kamilakesbi/fisher_medium'
-    split = "train"
-    dataloader_batch_size = 16
-    dataloader_num_workers = 8
-    preprocessing_num_workers = 24
-    streaming = True
+    parser = HfArgumentParser((ModelArguments, DataArguments))
+    model_args, data_args = parser.parse_args_into_dataclasses()
 
-    logger.debug('batch size: {}'.format(dataloader_batch_size))
+    # Dataset Processing Hyperparameters : 
+    dataset_name = str(data_args.per_device_eval_batch_size)
+    dataset_split_name = str(data_args.dataset_split_name)
+    per_device_batch_size = int(data_args.per_device_eval_batch_size)
+    dataloader_num_workers = int(data_args.dataloader_num_workers)
+    num_proc = int(data_args.num_proc)
+    streaming = str(data_args.streaming)
+
+    logger.debug('Per device batch size: {}'.format(per_device_batch_size))
     logger.debug('Data loader num workers: {}'.format(dataloader_num_workers))
-    logger.debug('Preprocessing num workers: {}'.format(preprocessing_num_workers))
+    if not streaming: 
+        logger.debug('Dataset loading num workers: {}'.format(dataloader_num_workers))
 
     # Load the different models: 
-    asr_model = "distil-whisper/distil-large-v3"
-    diarizer_model = "pyannote/speaker-diarization-3.1"
+    asr_model_name = str(model_args.asr_name_or_path)
+    diarizer_model_name = str(model_args.diarizer_name_or_path)
+    normalizer_name = str(model_args.normalizer_name_or_path)
 
     accelerator = Accelerator()
     device = accelerator.device
 
-    # Diarization LM parameters: 
+    # Load prompt options: 
     prompts_options = utils.PromptOptions()
 
-    asr_processor = WhisperProcessor.from_pretrained(asr_model, token=True)
-
-    attn_implementation = "sdpa" if is_torch_sdpa_available() else "eager"
+    # Load ASR Model: 
+    asr_processor = WhisperProcessor.from_pretrained(asr_model_name, token=True)
 
     asr_model = WhisperForConditionalGeneration.from_pretrained(
-        asr_model, 
+        asr_model_name, 
         token=True, 
-        attn_implementation=attn_implementation, 
+        attn_implementation=str(model_args.attn_implementation), 
     )
 
-    normalizer = WhisperTokenizer.from_pretrained("distil-whisper/distil-large-v3")
+    # Load diarization pipeline: 
+    diarization_pipeline = Pipeline.from_pretrained(diarizer_model_name)
 
-    diarization_pipeline = Pipeline.from_pretrained(diarizer_model).to(torch.device(device))
+    # Load Normalizer: 
+    normalizer = WhisperTokenizer.from_pretrained(str(normalizer_name))
 
     sample_rate = asr_processor.feature_extractor.sampling_rate
 
+    # Prepare models for accelerate: 
     diarization_pipeline, asr_model, asr_processor, normalizer = accelerator.prepare(diarization_pipeline, asr_model, asr_processor, normalizer)
 
+    # Prepare Processor: 
     processor = Processor(
         diarization_pipeline, 
         asr_model, 
@@ -73,20 +160,21 @@ if __name__ == '__main__':
         prompts_options
     )
 
+
     with accelerator.main_process_first(): 
         if streaming: 
             raw_dataset = load_dataset(
                 dataset_name, 
-                split='train', 
+                split=dataset_split_name, 
                 streaming=True, 
                 num_proc=None,
             )
         else: 
             raw_dataset = load_dataset(
                 dataset_name, 
-                split='train', 
+                split=dataset_split_name, 
                 streaming=False, 
-                num_proc=preprocessing_num_workers,
+                num_proc=num_proc,
             )
 
     label_dataset = raw_dataset.select_columns(['timestamps_start', 'timestamps_end', 'speakers', 'transcripts'])
@@ -104,7 +192,7 @@ if __name__ == '__main__':
     # Define Data Loaders: 
     audio_dataloader = DataLoader(
             audio_dataset,
-            batch_size=dataloader_batch_size,
+            batch_size=per_device_batch_size,
             collate_fn=audio_data_collator,
             num_workers=dataloader_num_workers,
             pin_memory=True,
@@ -112,7 +200,7 @@ if __name__ == '__main__':
     
     labels_dataloader = DataLoader(
         label_dataset,
-        batch_size=dataloader_batch_size * accelerator.num_processes,
+        batch_size= per_device_batch_size * accelerator.num_processes,
         collate_fn=labels_data_collator,
         num_workers=dataloader_num_workers,
     )
@@ -122,7 +210,7 @@ if __name__ == '__main__':
 
     processed_dataset = Dataset.from_dict({"ref_diarized_text": [], "ref_text": [], "ref_labels": [], "hyp_text": [], "ref_labels": []})
 
-    print('Entering dataloder loop: ')
+    logger.debug('Entering dataloder loop: ')
 
     start_time = time.perf_counter()
     for step, (audio_batch, labels_batch) in tqdm(enumerate(zip(audio_dataloader,labels_dataloader))):
@@ -165,7 +253,9 @@ if __name__ == '__main__':
 
         accelerator.wait_for_everyone()
 
-    processed_dataset.push_to_hub('kamilakesbi/test', private=True)
+
+    if str(data_args.push_to_hub): 
+        processed_dataset.push_to_hub(str(data_args.output_hub_repository), private=True)
 
 
 
